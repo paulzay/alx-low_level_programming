1985654f68d8f10a4279f22ac960b94865c57015
Paul Ogolla <paulotieno2@gmail.com>
2023-09-07 11:33:19 +0300

    fix typo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/*
+ * return type int
+ * main - which is the entry point
+ * method puts that will output string with new line
+ * return value of type int
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
