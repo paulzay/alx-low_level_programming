b54332e02e5e9cc2725fdb3f5a0874300c454298
Paul Ogolla <paulotieno2@gmail.com>
2023-09-08 20:20:54 +0300

    task 8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: tba
+ *
+ * Return: Always Successful (0)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
