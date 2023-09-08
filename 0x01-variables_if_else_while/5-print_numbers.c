fb1f12b0db708a3b7e2c6b7f963149e4fb9f76a0
Paul Ogolla <paulotieno2@gmail.com>
2023-09-08 19:36:52 +0300

    task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: tba
+ *
+ * Return: Always Successful (0)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+
+	return (0);
+}
