40bfd8c075e240668bf80072acb143d2482540f3
Paul Ogolla <paulotieno2@gmail.com>
2023-09-08 19:59:09 +0300

    task 7

---
+++ 0x01-variables_if_else_while/6-print-numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: tba
+ *
+ * Return: Always successful (0)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
