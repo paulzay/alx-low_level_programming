9f53bc4d12863c93e3eaa339622ac1c790fff33f
Paul Ogolla <paulotieno2@gmail.com>
2023-09-08 18:37:58 +0300

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: tba
+ *
+ * Return: Always success (0)
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
