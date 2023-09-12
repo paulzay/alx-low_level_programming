d18ae74492807eab4f2915df41ee21b417aca26b
Paul Ogolla <paulotieno2@gmail.com>
2023-09-12 22:47:54 +0300

    task 8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * print_last_digit - printing -1 digit
+ * @i: the input
+ * Return: last_digit
+*/
+
+int print_last_digit(int i)
+{
+	int last_digit = i % 10;
+	return (last_digit);
+}
