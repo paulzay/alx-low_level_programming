a0dda61f95c07899921fabb5faefdd6d31d8a3b8
Paul Ogolla <paulotieno2@gmail.com>
2023-09-12 20:23:27 +0300

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print a to z
+*/
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
