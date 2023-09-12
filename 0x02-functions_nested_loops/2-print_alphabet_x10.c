acf0fcfa5ef1626701b1ef8d1c79881c55de999f
Paul Ogolla <paulotieno2@gmail.com>
2023-09-12 20:57:37 +0300

    task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,14 @@
  * print_alphabet_10x - print a-z ten times
 */
 
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 	int c;
 	int count;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count <= 9; ++count)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; ++c)
 		{
 			_putchar(c);
 		}
