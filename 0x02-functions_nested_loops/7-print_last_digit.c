f214af7c8ef30e1504d429f64d415ec79f8d4ef7
Paul Ogolla <paulotieno2@gmail.com>
2023-09-12 22:53:33 +0300

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,13 +10,13 @@ int print_last_digit(int i)
 {
 	int last_digit;
 
-	if (n < 0)
+	if (i < 0)
 	{
-		last_digit = -1 * (n % 10);
+		last_digit = -1 * (i % 10);
 	}
 	else
 	{
-		last_digit = n % 10;
+		last_digit = i % 10;
 	}
 
 	_putchar(last_digit + '0');
