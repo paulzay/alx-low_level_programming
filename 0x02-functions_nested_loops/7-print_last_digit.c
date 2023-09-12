91d0a3f763e779526b4cf8a955b258f79160bcf5
Paul Ogolla <paulotieno2@gmail.com>
2023-09-12 22:52:33 +0300

    task 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,18 @@
 
 int print_last_digit(int i)
 {
-	int last_digit = i % 10;
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = -1 * (n % 10);
+	}
+	else
+	{
+		last_digit = n % 10;
+	}
+
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
