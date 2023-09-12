92f6fa78bfaee8b36ac4d3907a2a683302ec835a
Paul Ogolla <paulotieno2@gmail.com>
2023-09-12 21:08:45 +0300

    task 4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *
+ * @c: the input
+ *
+ * Return: Success(0) or Error(1)
+*/
+
+int _islower(int c)
+{
+	if (c == toLower(c))
+	{
+		return (1);
+	}
+	return (0);
+
+}
