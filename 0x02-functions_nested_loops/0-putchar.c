5a51fa9b0e6b4c4848fc5945622d7e7021df5e10
Paul Ogolla <paulotieno2@gmail.com>
2023-09-12 20:09:53 +0300

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,22 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - entry point
- *
  * description: tba
- *
  * Return: Always Success (0)
 */
 
 int main(void)
 {
-  char str[] = "_putchar";
-  int c;
-
-  for (c = 0; c < 8; c++)
-  {
-    _putchar(str[c]);
-
-  }
-  _putchar('\n');
-  
-  return (0);
+	char str[] = "_putchar";
+	int c;
+	
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(str[c]);
+	}
+	
+	_putchar('\n');
+	
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
-void printput(String)
-{ 
-  printf("putchar\n");
-  return;
-}
+int _putchar(char c);
+
+#endif
