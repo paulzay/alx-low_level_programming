67481f3e6b6e02af6123a1a6ebc52cedef384bcf
Paul Ogolla <paulotieno2@gmail.com>
2023-09-12 22:30:25 +0300

    task 6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - print signs
+ *
+ * @n: the input
+ *
+ * Return: 0 or 1 or -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43 + 0);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + 48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45 + 0);
+		return (-1);
+	}
+}
