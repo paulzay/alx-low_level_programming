738a032ddc8e43b4753ce4b175f8f4c0194f6449
Paul Ogolla <paulotieno2@gmail.com>
2023-09-14 23:47:53 +0300

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,15 @@ int main(void)
 	{
 		if (num % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (num % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 			printf("%d ", num);
