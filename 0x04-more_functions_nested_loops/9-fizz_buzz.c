4d21b1b1ee812968feeeaa72741c5ada5f9c6056
Paul Ogolla <paulotieno2@gmail.com>
2023-09-14 23:51:36 +0300

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0)
+		if (num % 3 == 0 && !(num % 5 == 0))
 		{
 			printf("Fizz");
 		}
-		else if (num % 5 == 0)
+		else if (num % 5 == 0 && !(num %3 == 0))
 		{
 			printf("Buzz");
 		}
-		else if (num % 15 == 0)
+		else if (num % 3 == 0 && num % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
