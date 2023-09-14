291b0e08a16205a352dc2fd62dfd21cb51b08493
Paul Ogolla <paulotieno2@gmail.com>
2023-09-14 19:49:51 +0300

    task 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if its digit
+ * @c: input
+ * Return: 0
+*/
+
+int _isdigit(int c)
+{
+	if (c >=1 || <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
