ff4c332635e0b5fc2d97f800768d74426f74e8bf
Paul Ogolla <paulotieno2@gmail.com>
2023-09-14 20:43:28 +0300

    print nums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - fn
+ * Description: tba
+ * Return: 0
+*/
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
