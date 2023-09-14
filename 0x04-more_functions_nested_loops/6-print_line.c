ddd3fc01862d8fe554e6bc4a5f8758d1227c91b5
Paul Ogolla <paulotieno2@gmail.com>
2023-09-14 22:01:28 +0300

    task done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - fn
+ * @n: input
+ * Return: 0
+*/
+
+void print_line(int n)
+{
+	int count;
+
+	if (n >= 1)
+	{
+		for (count = 0; count <= n-1; count++)
+		{
+			_putchar('_');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
