cf32de5f882abc0d60c988dc0939323fd866e635
Paul Ogolla <paulotieno2@gmail.com>
2023-09-14 19:39:49 +0300

    first task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks uppercase
+ * @c: input
+ * Return: 0
+*/
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
