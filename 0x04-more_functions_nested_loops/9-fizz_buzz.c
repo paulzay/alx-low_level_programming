554651b5b7415e4627fe8ec024ae8db2ab9cfc74
Paul Ogolla <paulotieno2@gmail.com>
2023-09-14 23:32:59 +0300

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * fizzbuzz - fn
+ * Description: tba
+ * Return: 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (num % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+			printf("%d ", num);
+	}
+	return (0);
+}
