c57bf48ea2a461f67da425da8da7e65162d0690b
Paul Ogolla <paulotieno2@gmail.com>
2023-09-14 19:44:57 +0300

    fixer

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,7 +8,7 @@
 
 int _isupper(int c)
 {
-	if (isupper(c))
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,6 @@
 
 #include "stdio.h"
 
-int _putchar(char c);
 int _isupper(int c);
 
 #endif
