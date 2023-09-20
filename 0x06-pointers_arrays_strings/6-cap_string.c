c924ea339a449843bd94d4d3403515516366c4f4
Paul Ogolla <paulotieno2@gmail.com>
2023-09-20 23:03:16 +0300

    add files

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * cap_string - fn
+ * @str: input
+ * Return: 0
+*/
+
+char *cap_string(char *atr);
+{
+
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * leet - fn
+ * @str: input
+ * Return: 0
+*/
+
+char *leet(char *atr);
+{
+	int i, j;
+	j = 0;
+
+	while
+}
