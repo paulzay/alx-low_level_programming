a16eb14d69301d1328b56c12704207c26764711d
Paul Ogolla <paulotieno2@gmail.com>
2023-09-20 23:35:40 +0300

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,5 +8,31 @@
 
 char *cap_string(char *atr);
 {
+    int i, j;
+    char c[] = " \t\n,;.!?\"(){}";
 
+    i = 0;
+    
+    while (*(str + i))
+    {
+      if (*(str + i) >= 'c' && *(str + i) <= 'z')
+      {
+        if (i == 0)
+        {
+          *(str + i) -= 'a' - 'A';
+        }
+        else
+        {
+          for (j = 0; j <= 12; j++)
+          {
+            if (c[j] == *(str + i - 1))
+            {
+              *(str + i) -= 'a' - 'A';
+            }
+          }
+        }
+      }
+      i++;
+    }
+    return(str);
 }
