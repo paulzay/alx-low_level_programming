da4743c8b71ec8b34a48ccdfa513177ee5c330b7
Paul Ogolla <paulotieno2@gmail.com>
2023-09-20 19:21:16 +0300

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,20 +12,20 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = 0;
-	while (src[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
 	j = 0;
-	while (dest[j] != '\0')
+	while (src[j] != '\0')
 	{
-		src[i] = dest[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
 
-	src[i] = '\0';
+	dest[i] = '\0';
 
-	return (src);
+	return (dest);
 }
