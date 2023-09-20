b7049b101eacb781ae4695cd2565d2f64804523e
Paul Ogolla <paulotieno2@gmail.com>
2023-09-20 23:43:54 +0300

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,33 +6,32 @@
  * Return: 0
 */
 
-char *cap_string(char *atr);
+char *cap_string(char *str);
 {
-    int i, j;
-    char c[] = " \t\n,;.!?\"(){}";
+	int i, j;
+	char c[] = " \t\n,;.!?\"(){}";
 
-    i = 0;
-    
-    while (*(str + i))
-    {
-      if (*(str + i) >= 'c' && *(str + i) <= 'z')
-      {
-        if (i == 0)
-        {
-          *(str + i) -= 'a' - 'A';
-        }
-        else
-        {
-          for (j = 0; j <= 12; j++)
-          {
-            if (c[j] == *(str + i - 1))
-            {
-              *(str + i) -= 'a' - 'A';
-            }
-          }
-        }
-      }
-      i++;
-    }
-    return(str);
+	i = 0;
+	while (*(str + i))
+	{
+		if (*(str + i) >= 'c' && *(str + i) <= 'z')
+		{
+			if (i == 0)
+			{
+				*(str + i) -= 'a' - 'A';
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (c[j] == *(str + i - 1))
+					{
+						*(str + i) -= 'a' - 'A';
+					}
+				}
+			}
+		}
+		i++;
+	}
+	return (str);
 }
