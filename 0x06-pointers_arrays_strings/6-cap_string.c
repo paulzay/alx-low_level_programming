74477210ba707760388f539b676b366a7103effb
Paul Ogolla <paulotieno2@gmail.com>
2023-09-21 22:38:58 +0300

    betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,18 +9,18 @@
 char *cap_string(char *str)
 {
 	int i;
-  
-  i = 0;
-  
-  while (str[i] != '\0')
-  {
-    if (str[i] >= 'a' && str[i] <= 'z')
-    {
-      if (i == 0)
-      {
-        str[i] -= 32;
-      }
-      if (str[i - 1] == 32 || str[i - 1] == 9 || str[i - 1] == 10 ||
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (i == 0)
+			{
+				str[i] -= 32;
+			}
+			if (str[i - 1] == 32 || str[i - 1] == 9 || str[i - 1] == 10 ||
 				str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46 ||
 				str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 ||
 				str[i - 1] == 40 || str[i - 1] == 41 || str[i - 1] == 123 ||
@@ -28,8 +28,8 @@ char *cap_string(char *str)
 			{
 				str[i] -= 32;
 			}
-    }
-    i++;
-  }
-  return (str);
+		}
+		i++;
+	}
+	return (str);
 }
