59445402034df3c097995f518762fe3c229042ae
Paul Ogolla <paulotieno2@gmail.com>
2023-10-03 23:08:45 +0300

    test

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - fn returns ptr to space in mem
+ * @str: input
+ * Return: of type char
+*/
+char *_strdup(char *str)
+{
+	char *st;
+	int i = 0;
+	int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	st = malloc(sizeof(char) * (j + i));
+
+	if (st == NULL)
+		return (NULL);
+
+	while (str != '\0')
+	{
+		st[i] = str[i];
+		i++;
+	}
+
+	return (st);
+}
