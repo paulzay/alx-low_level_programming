e6cff9166dfb2d4d9ee949f8d9aaa3728d790e0f
Paul Ogolla <paulotieno2@gmail.com>
2023-10-03 22:20:48 +0300

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_array - fn that create arrays of chars
+ * @size: size of array
+ * @c: char
+ * Return: of type char
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
+
+}
