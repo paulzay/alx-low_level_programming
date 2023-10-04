938a6d8cf3398017b0cdd47fc9514ae63ccafa8b
Paul Ogolla <paulotieno2@gmail.com>
2023-10-04 23:37:44 +0300

    concat strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,32 +14,40 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	int lengths1 = 0;
 	int lengths2 = 0;
-	int strlngth;
 	char *output;
+	int strlngth = 0;
 
 	if (s1 != NULL)
 	{
 		i = 0;
-		while (s1[i] != '\0')
+		while (s1[lengths1] != '\0')
 		{
 			lengths1++;
-			i++;
 		}
 	}
 
 	if (s2 != NULL)
 	{
 		j = 0;
-		while (s2[j] != '\0')
+		while (s2[lengths2] != '\0')
 		{
 			lengths2++;
-			j++;
 		}
 	}
 
-	strlngth = lengths2 + lengths1;
-	output = (char *)malloc(sizeof(char) * (strlngth + 1));
+	output = malloc((lengths1 * sizeof(char)) + (lengths2 + 1) * sizeof(char));
 
 	if (output == NULL)
 		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		output[i] = s1[i];
+
+	for (j = 0; s[j] != '\0'; j++)
+		output[i] = s2[j];
+		i++;
+
+	output[i] = '\0';
+
+	return (output);
 }
