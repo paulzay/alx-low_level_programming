3245e0e9a1cae0d140e06d211fcb591a7e932f4b
Paul Ogolla <paulotieno2@gmail.com>
2023-10-04 18:28:43 +0300

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - fn
+ * @argc: no of arguments
+ * @argv: array
+ * Return: 0
+*/
+
+int main(int argc, char __attribut__((unused)) * argv[])
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < argv)
+	{
+		j = i;
+		i++;
+	}
+	printf("%d\n", j);
+	return (0);
+}
