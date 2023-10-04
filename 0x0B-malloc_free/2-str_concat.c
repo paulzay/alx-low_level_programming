85015580c5e4323afc0222e59deedd062225e532
Paul Ogolla <paulotieno2@gmail.com>
2023-10-04 23:38:42 +0300

    concat strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -43,7 +43,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0'; i++)
 		output[i] = s1[i];
 
-	for (j = 0; s[j] != '\0'; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 		output[i] = s2[j];
 		i++;
 
