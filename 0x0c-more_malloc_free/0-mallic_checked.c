8163ebde9caae8c11215bf3cf88c0a21ac911fb6
Paul Ogolla <paulotieno2@gmail.com>
2023-10-05 21:59:09 +0300

    task 0

---
+++ 0x0c-more_malloc_free/0-mallic_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - fn that allocates memory w malloc
+ * @b: int
+ * Return: void
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *n;
+
+	n = malloc(n);
+
+	if (n == NULL)
+		exit(98);
+}
