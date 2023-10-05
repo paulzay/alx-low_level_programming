8072fd7a2c3cc82aa9aea5fe229458efb10bc79e
Paul Ogolla <paulotieno2@gmail.com>
2023-10-05 22:16:21 +0300

    task 0 done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,22 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - fn that allocates memory w malloc
  * @b: int
  * Return: void
 */
+
 void *malloc_checked(unsigned int b)
 {
 	void *n;
 
-	n = malloc(n);
+	n = malloc(sizeof(n) * b);
 
 	if (n == NULL)
+	{
 		exit(98);
+	}
+	else
+		return (n);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
 void *malloc_checked(unsigned int b);
 
