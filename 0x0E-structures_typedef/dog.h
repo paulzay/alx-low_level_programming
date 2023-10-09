254e424ac0e7a32e606a636d30693052c05748a6
Paul Ogolla <paulotieno2@gmail.com>
2023-10-10 00:05:11 +0300

    add typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,14 @@
  * @owner: dog owner
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
