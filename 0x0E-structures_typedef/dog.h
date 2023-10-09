b2f8dead6d5f3ce070387298a17e3ab174b14bcc
Paul Ogolla <paulotieno2@gmail.com>
2023-10-09 22:39:10 +0300

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+/**
+ * struct dog - struct for dog props
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
