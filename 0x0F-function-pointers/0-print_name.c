bd68541c77fde94a8883f91d6941ec1de6a9c275
Paul Ogolla <paulotieno2@gmail.com>
2023-10-11 22:39:08 +0300

    print name

---
+++ 0x0F-function-pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - fn
+ *
+ * @name: ptr to name
+ *
+ * @f: ptr to function
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
