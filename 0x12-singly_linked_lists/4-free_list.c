e703bf501524d69016a81f2c3d01e75d3772e197
Paul Ogolla <paulotieno2@gmail.com>
2023-10-20 01:16:14 +0300

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_list - free the list
+ * @head: Head
+ * Return: nothing
+*/
+void free_list(list_t *head)
+{
+	list_t *curr;
+
+	while ((curr = head) != NULL)
+		head = (*head).next;
+		free(curr.str);
+		free(curr);
+}
