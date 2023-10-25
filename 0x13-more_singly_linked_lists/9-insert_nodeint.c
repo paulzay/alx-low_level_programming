c99e81ce21b3b5973e6a29f6af9301af6b546ccf
Paul Ogolla <paulotieno2@gmail.com>
2023-10-25 23:19:06 +0300

    ctrl+z

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		currentNode = currentNode->next;
 		if (currentNode == NULL)
 		{
-			free(currentNode)
 			return (NULL);
 		}
 		i++;
