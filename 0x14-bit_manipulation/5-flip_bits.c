95a90d35e3ae6e9fc5afa30183ac5359f966b1e8
Paul Ogolla <paulotieno2@gmail.com>
2023-10-28 00:19:55 +0300

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - fn to flip
+ * @n: n
+ * @m: m
+ * Return: flipped
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int cnt;
+	unsigned long int diff;
+
+	diff = n ^ m;
+	cnt = 0
+	while (diff > 0)
+	{
+		count += diff & 1;
+		diff >>= 1;
+	}
+	return (count);
+}
