638fb8a55170f9b379b144c5e301fdb46e9a9d17
Paul Ogolla <paulotieno2@gmail.com>
2023-10-28 00:07:14 +0300

    task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - fn
+ * @n: int
+ * @index: index
+ * Return: index
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > sizeof(unsigned long int) * 8 - 1)
+		return (-1);
+
+	*n = *n | (1 << index);
+
+	return (1);
+}
