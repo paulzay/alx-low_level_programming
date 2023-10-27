a7300820b17cf8a8a680db9e86fa07e30210fb5c
Paul Ogolla <paulotieno2@gmail.com>
2023-10-28 00:12:56 +0300

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - fn
+ * @n: int
+ * @index: index
+ * Return: index
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > sizeof(unsigned long int) * 8 - 1)
+		return (-1);
+
+	*n = *n | ~(1 << index);
+
+	return (1);
+}
