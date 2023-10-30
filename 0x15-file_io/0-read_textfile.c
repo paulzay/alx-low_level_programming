50d64a2cdca9ed5edb11aeaebd86ea2d1051a7aa
Paul Ogolla <paulotieno2@gmail.com>
2023-10-31 00:30:54 +0300

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,20 +9,34 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
+	int fp, no_of_read, written;
+	char *buf;
 
-	char buf[1024];
+	buf = malloc(sizeof(*buf) * (letters + 1));
 
-	fp = fopen(filename, "r");
-	
-	if (fp == NULL || filename ==NULL)
+	if (buf == NULL || filename == NULL)
+	{
+		free(buf);
+		return (0);
+	}
+
+	fp = open(filename, O_RDONLY);
+
+	if (fp == -1)
 		return (0);
 
-	while ((letters = fread(buf, 1, sizeof(buf),fp) > 0))
-	{
-		fwrite(buf, 1, letters, stdout);
-	}
-	
-	fclose(fp);
-	return (0);
+	no_of_read = read(fp, buf, letters);
+
+	if (no_of_read == -1)
+		return (0);
+
+	buf[no_of_read] = '\0';
+
+	written = write(STDOUT_FILENO, buf, no_of_read);
+	if (written != no_of_read)
+		return (0);
+
+	free(buf);
+	close(fp);
+	return (no_of_read);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,14 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
