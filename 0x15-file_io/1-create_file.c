b6e7e0ff07c445286c22338b12517b1bcbf5b9ef
Paul Ogolla <paulotieno2@gmail.com>
2023-10-31 01:18:51 +0300

    ;

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		written = write(fp, text_content, count);
 
 		if (written == -1)
-			close(fp)
+			close(fp);
 			return (-1);
 	}
 	close(fp);
