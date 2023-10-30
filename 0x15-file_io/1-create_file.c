ba3c54bfdc3fa761484dbfdd149c9f7062995ecc
Paul Ogolla <paulotieno2@gmail.com>
2023-10-31 01:08:09 +0300

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - function
+ * @filename: pointer to file
+ * @text_content: content
+ * Return: 0
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp, written, count;
+
+	if (filename == NULL)
+		return (-1);
+	
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fp == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[count] != '\0')
+		{
+			count++;
+		}
+
+		written = write(fp, text_content, count);
+
+		if (written == -1)
+			return (-1);
+	}
+	close(fp);
+	return (1);
+
+}
