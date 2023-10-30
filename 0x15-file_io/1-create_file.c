219c00034f4c7a2dc2117f9aff215b768595a4be
Paul Ogolla <paulotieno2@gmail.com>
2023-10-31 01:12:10 +0300

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (fp == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
