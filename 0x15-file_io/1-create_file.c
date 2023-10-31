c2958a786b85ece98e16c6ff5acd41172098886b
Paul Ogolla <paulotieno2@gmail.com>
2023-10-31 13:56:11 +0300

    refactor

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,9 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int bytes_written;
 
+	if (filename == NULL)
+		return (-1)
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
@@ -25,5 +28,5 @@ int create_file(const char *filename, char *text_content)
 	if (bytes_written == -1)
 		return (-1);
 	close(fd);
-	return (0);
+	return (1);
 }
