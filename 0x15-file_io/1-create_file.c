1ddaab84566c09a1d94814eff854a9f20e020c81
Paul Ogolla <paulotieno2@gmail.com>
2023-10-31 14:08:18 +0300

    snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,23 +10,24 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int bytes_written;
+	int bytes_written, count;
 
 	if (filename == NULL)
 		return (-1);
 
+	/* int open ( char *path, int flags ) */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		return (-1);
-	}
-	else
-	{
-		bytes_written = write(fd, text_content, sizeof(text_content) - 1);
+		while (text_content[count] != '\0')
+		{
+			count++;
+		}
+		bytes_written = write(fd, text_content, count);
 
 		if (bytes_written == -1)
 		{
