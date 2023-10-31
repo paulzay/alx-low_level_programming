974ee9b3cb87ad9c7d212c916593df5689ae02fa
Paul Ogolla <paulotieno2@gmail.com>
2023-10-31 14:01:49 +0300

    failing cases

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
 	if (fd == -1)
 		return (-1);
 
@@ -23,10 +24,17 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	bytes_written = write(fd, text_content, sizeof(text_content) - 1);
+	else
+	{
+		bytes_written = write(fd, text_content, sizeof(text_content) - 1);
 
-	if (bytes_written == -1)
-		return (-1);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
 	close(fd);
 	return (1);
 }
