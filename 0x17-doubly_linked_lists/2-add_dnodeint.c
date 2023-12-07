102f23530994e21fbfc3bd429a323793c56d2885
Paul Ogolla <paulotieno2@gmail.com>
2023-12-08 00:40:43 +0300

    sf

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->prev = NULL;
 
 	if (*head)
-		(*head)->prev = NULL;
+		(*head)->prev = newNode;
 
 	*head = newNode;
 	return (newNode);
