59ac03b01e84eeba6fad99387381145c6bdd7498
Paul Ogolla <paulotieno2@gmail.com>
2023-12-07 16:34:11 +0300

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - function to print lists
+ * @h: the head node
+ * Return: number of nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
