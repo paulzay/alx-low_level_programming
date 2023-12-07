4c5a2ead9c62c8f445999ad7a5eafbd4c9f22b6b
Paul Ogolla <paulotieno2@gmail.com>
2023-12-07 20:01:00 +0300

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
