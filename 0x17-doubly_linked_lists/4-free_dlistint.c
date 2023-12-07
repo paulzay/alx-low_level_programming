13f6196c610280ad9f6630695c7bdfe0a940e721
Paul Ogolla <paulotieno2@gmail.com>
2023-12-07 23:14:24 +0300

    fix betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -5,13 +5,14 @@
  * Description: frees
  * Return: nothing
 */
-void free_dlistint(dlistint_t *head){
-    dlistint_t *currentNode;
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *currentNode;
 
-    while (head != NULL)
-    {
-        currentNode = head;
-        head = head->next;
-        free(currentNode);
-    }
+	while (head != NULL)
+	{
+		currentNode = head;
+		head = head->next;
+		free(currentNode);
+	}
 }
