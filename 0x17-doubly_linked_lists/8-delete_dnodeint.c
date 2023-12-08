a5f40ba4a73542ef9fd54eab45f5bd2ba262e35c
Paul Ogolla <paulotieno2@gmail.com>
2023-12-08 05:52:57 +0300

    start

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,5 +3,10 @@
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *currentNode = *head;
+	dlistint_t tempNode = NULL;
+	unsigned int n;
 
+	if (*head == NULL)
+		return (-1);
 }
