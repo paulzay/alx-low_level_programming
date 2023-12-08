31140f7599823d2718ef8e201ade10ad80853810
Paul Ogolla <paulotieno2@gmail.com>
2023-12-08 22:45:36 +0300

    8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,12 +1,33 @@
 #include "lists.h"
 /**
+ * delete_dnodeint_at_index - fn
+ * @head: head
+ * @index: index
+ * Return: 1 or -1
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *currentNode = *head;
-	dlistint_t tempNode = NULL;
-	unsigned int n;
+	dlistint_t *tempNode = *head;
+	unsigned int n = 0;
 
 	if (*head == NULL)
 		return (-1);
+
+	if (currentNode->next == NULL)
+	{
+		return (1);
+	}
+	while (currentNode)
+	{
+		if (index == n)
+		{
+			tempNode = currentNode->next;
+			currentNode = tempNode;
+		}
+
+		currentNode = currentNode->next;
+	}
+	free(currentNode);
+	return (1);
 }
