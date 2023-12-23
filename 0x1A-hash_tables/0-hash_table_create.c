ad7160205c27a975ac9baa97763fbad5e6783a6c
Paul Ogolla <paulotieno2@gmail.com>
2023-12-23 03:32:51 +0300

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,24 +7,24 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  unsigned long int i;
+	unsigned long int i;
+	hash_table_t *table;
 
-  hash_table_t *table;
-  table = malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 
-  if (table == NULL)
-    return (NULL);
+	if (table == NULL)
+		return (NULL);
 
-  table->size = size;
-  table->array = malloc(sizeof(hash_node_t *) * size);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
 
-  if (table->array == NULL)
-    return (NULL);
+	if (table->array == NULL)
+		return (NULL);
 
-  for (i=0;i < size;i++)
-  {
-    table->array[i] = NULL;
-  }
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
 
-  return (table);
+	return (table);
 }
